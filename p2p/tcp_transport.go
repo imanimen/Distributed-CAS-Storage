e34a301c237374b2dc52cb8d3c4ff20e497ddb8c
Iman Imen <imanimen81@gmail.com>
2026-02-12 14:34:30 +0330

    chore(tests): test the TCP transport

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,10 +1,20 @@
 package p2p
 
-import "net"
+import (
+	"net"
+	"sync"
+)
 
 type TCPTransport struct {
-	listenAddr string
-	listener   net.Listener
-	
+	listenAddress string
+	listener      net.Listener
+
+	mu    sync.RWMutex
 	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
 }
